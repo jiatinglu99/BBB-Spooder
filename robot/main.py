0cbbaefdfe122b212a27cd97ddaeb317a75bc714
danielVebman <danveb800@gmail.com>
2019-05-02 19:47:01 -0400

    Added TCP server on background thread and handling. Tested parts.

--- robot/main.py
+++ robot/main.py
@@ -1,7 +1,7 @@
 from driver.sensor.central import SensorCentral
 from driver.esc.central import ESCCentral
 from driver.servo.central import ServoCentral
-from driver.remote_control import RemoteControl 
+from driver.remote_control import RemoteControl
 
 class Robot:
     def __init__(self):
@@ -10,8 +10,15 @@ class Robot:
         self.servo_central = ServoCentral()
         self.remote_control = RemoteControl(handler=self)
 
-    def handle_remote_command(self, command):
-        print('Got command:', command)
+    def controller_connected(self):
+        pass
+
+    # Like GOD
+    def controller_commanded(self, type, body):
+        pass
+
+    def controller_disconnected(self):
+        pass
 
 
 if __name__ == "__main__":
