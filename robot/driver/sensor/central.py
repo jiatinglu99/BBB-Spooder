f8e303ff7f70efedc7e29ac841315987c6fa28b4
jiatinglu99 <jiatinglu99@gmail.com>
2019-05-05 16:58:06 -0400

    Sensor Central gyro incorporate

--- robot/driver/sensor/central.py
+++ robot/driver/sensor/central.py
@@ -1,7 +1,10 @@
-from gps import GPSSensor
-from current_monitor import CurrentSensor
-from gyroscope import GyroscopeSensor
-from proximity import ProximitySensor
+from .gps import GPSSensor
+from .current_monitor import CurrentSensor
+from .gyroscope import GyroscopeSensor
+from .proximity import ProximitySensor
+
+import time
+import threading
 
 class SensorCentral:
     def __init__(self):
@@ -9,3 +12,23 @@ class SensorCentral:
         self.current = CurrentSensor()
         self.gyroscope = GyroscopeSensor()
         self.proximity = ProximitySensor()
+
+        # sensor threads
+        self.threads = []
+        #self.threads.append(threading.Thread(target = self.gps.thread_update()))
+        #self.threads.append(threading.Thread(target = self.current.thread_update()))
+        self.threads.append(threading.Thread(target = self.gyroscope.thread_update()))
+        #self.threads.append(threading.Thread(target = self.proximity.thread_update()))
+
+        # start threading
+        for t in self.threads:
+            t.start()
+
+    def getGPS(self):
+        pass
+
+    def getCurrent(self):
+        pass
+
+    def getGyroscope(self):
+        return self.gyroscope.get_data()
