2f599a6e1aab498a8071ffa7f495860cf260cb01
jiatinglu99 <jiatinglu99@gmail.com>
2019-05-05 22:25:34 -0400

    minor servo update

--- robot/driver/servo/servo.py
+++ robot/driver/servo/servo.py
@@ -2,8 +2,7 @@
 Drivers for motors controlled by SSC
 Servo Type 0: Original Virtual Servo 
 Servo Type 1: SG90 Tower Pro: http://www.ee.ic.ac.uk/pcheung/teaching/DE1_EE/stores/sg90_datasheet.pdf
-Servo Type 2: KRS-2552 or KRS-2572 in PWM mode: http://kondo-robot.com/w/wp-content/uploads/KRS-series_manual_Download-En.pdf
-Servo Type 3: PLACEHOLDER
+
 """
 
 class Servo:
@@ -32,29 +31,5 @@ class Servo:
             self.voltageMax = 6.0 #V
             self.torque = 2.5 #kg-cm
             self.weight = 14.7 #g
-        elif self.type == Servo.SERVO_KRS2552:
-            self.degreeMin = 
-            self.degreeMax = 
-            self.pulseMin = 700
-            self.pulseMax = 2300
-            self.pwmPeriod = 
-            self.maxFreq = 
-            self.minFreq = 
-            self.voltageMin = 
-            self.voltageMax = 
-            self.torque = 
-            self.weight = 
-        elif self.type == Servo.SERVO_KRS2572:
-            self.degreeMin = 
-            self.degreeMax = 
-            self.pulseMin = 
-            self.pulseMax = 
-            self.pwmPeriod = 
-            self.maxFreq = 
-            self.minFreq = 
-            self.voltageMin = 
-            self.voltageMax = 
-            self.torque = 
-            self.weight = 
         else:
             print('Invalid servo type!!!')
