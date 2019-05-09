7f066a48444c7b84f5e0df6b155b03947905b3f5
jiatinglu99 <jiatinglu99@gmail.com>
2019-05-09 16:41:06 -0400

    minor servo update

--- robot/driver/servo/LSS.py
+++ robot/driver/servo/LSS.py
@@ -1,24 +1,45 @@
 """
-Drivers for motors controlled by SSC
+Drivers for LSS Servo
 
 """
+import serial
+import time
 
 class Servo:
     def __init__(self, ID, polarity = True, offset = 0):
         self.ID = ID
-        self.currentPosition = 0
-        self.desiredPosition = 0
+        self.currentPosition = 0.0
+        self.desiredPosition = 0.0
         self.currentAmp = 0
         self.polarity = polarity
         self.offset = offset
-
-    def getCommandPositionStr(self, degree): 
-        self.desiredPosition = int(degree*10)
-        return "#{}D{}\r".format(self.ID, self.desiredPosition)
-
+        
+    def getPositionQueryStr(self):
+        return "#{}QD/r".format(self.ID).encode()
+    
+    def updateCurrentPosition(self, degree): #degree in 1
+        self.currentPosition = degree
+    
+    def updateDesiredPosition(self, degree): #degree in 1
+        self.desiredPosition = degree
+    
+    def getCommandStr(self): 
+        return "#{}D{}\r".format(self.ID, int(self.desiredPosition*10)).encode()
+    
     def getCurrentAmp(self):
         return self.currentAmp
     
     def updateAmp(self, amp):
         self.currentAmp = amp
         return None
+        
+def test():
+    sp = serial.Serial('/dev/ttyO1', 115200)
+    servo = Servo(1)
+    while True:
+        sp.write(servo.getPositionQueryStr())
+        time.sleep(0.1)
+        reading = sp.readline().decode('utf-8')
+        print(reading)
+        time.sleep(1)
+        
--- robot/driver/servo/leg.py
+++ robot/driver/servo/leg.py
@@ -3,13 +3,15 @@ from robot.driver.servo.LSS import Servo
 
 class Leg:
     def __init__(self, XID, YID, ZID):
-        servos = []
+        self.servos = []
         # base leg for Yaw
-        servos.append(Servo(XID, polarity = True, offset = 0))
+        self.servos.append(Servo(XID, polarity = True, offset = 0))
         # secondary leg for Pitch1
-        servos.append(Servo(YID, polarity = True, offset = 0))
+        self.servos.append(Servo(YID, polarity = True, offset = 0))
         # tertiary leg for Pitch2
-        servos.append(Servo(ZID, polarity = True, offset = 0))
+        self.servos.append(Servo(ZID, polarity = True, offset = 0))
+        
+        IDlist = [XID, YID, ZID]
         
     def getCommandStr(self):
-        commandCollection = self.servoX
+        return "{}{}{}{}".format(self.servos[0].getCommandStr)
