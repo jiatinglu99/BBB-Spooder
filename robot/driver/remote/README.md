f4a2bd4bfacd94124d61413b8dc1a4f3cda0ee0d
Nishir Shelat <nshelat@usc.edu>
2020-03-06 17:24:16 -0800

    Add event listeners for the right pad's buttons

---
+++ robot/driver/remote/README.md
@@ -0,0 +1,3 @@
+# Controlling the Spider
+## Using the DualShock 4 Wireless Controller
+We use ds4drv to connect to the controller via bluetooth. Run `ds4drv --hidraw --dameon` to start searching for the controller. Hold down the middle PlayStation button and the Share button at the same time to pair the controller. The controller is connected when the LED on the back stays lit up.
--- robot/driver/remote/central.py
+++ robot/driver/remote/central.py
@@ -4,5 +4,17 @@ class MyController(Controller):
     def __init__(self, **kwargs):
         Controller.__init__(self, **kwargs)
 
+    def on_triangle_release(self):
+           print("TRIANGLE released")
+
+    def on_circle_release(self):
+           print("CIRCLE released")
+
+    def on_x_release(self):
+           print("X released")
+
+    def on_square_release(self):
+           print("SQUARE released")
+
 controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=True)
-controller.listen()
+# controller.listen()
