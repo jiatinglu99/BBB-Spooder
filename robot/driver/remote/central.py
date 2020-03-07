ce4690ba3b3ff468633ef1c0202a6e3388124f86
Nishir Shelat <nshelat@usc.edu>
2020-03-06 16:48:54 -0800

    Change remote_central to controller

--- robot/driver/remote/central.py
+++ robot/driver/remote/central.py
@@ -1,8 +1,8 @@
 from pyPS4Controller.controller import Controller
 
-class RemoteCentral:
+class MyController(Controller):
     def __init__(self, **kwargs):
         Controller.__init__(self, **kwargs)
 
-remote_central = RemoteCentral(interface="/dev/input/js0", connecting_using_ds4drv=True)
-remote_central.listen()
+controller = MyController(interface="/dev/input/js0", connecting_using_ds4drv=True)
+controller.listen()
